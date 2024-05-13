a025adde953296835b28378297750a429add0cb8
Emandleni <emandlenimoyo@gmail.com>
2024-05-13 22:55:34 +0200

    Capture and admin tabs defined.

--- lib/main.dart
+++ lib/main.dart
@@ -34,6 +34,7 @@ class _ReviewFormState extends State<ReviewForm> {
   String _selectedSubject = 'Suggestion';
   String _message = '';
   bool _isLoading = false;
+  int _selectedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -41,71 +42,99 @@ class _ReviewFormState extends State<ReviewForm> {
       appBar: AppBar(
         title: Text('Review Form'),
       ),
-      body: LoadingOverlay(
-        isLoading: _isLoading,
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Subject',
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+      body: _selectedIndex == 0 ? _buildReviewForm() : _buildAdminPage(),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.camera_alt),
+            label: 'Capture',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.admin_panel_settings),
+            label: 'Admin',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.blue,
+        onTap: _onTabSelected,
+      ),
+    );
+  }
+
+  Widget _buildReviewForm() {
+    return LoadingOverlay(
+      isLoading: _isLoading,
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Subject',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              DropdownButtonFormField<String>(
+                value: _selectedSubject,
+                onChanged: (String? value) {
+                  setState(() {
+                    _selectedSubject = value!;
+                  });
+                },
+                items: ['Suggestion', 'Compliment', 'Bug', 'Feature Request']
+                    .map((String subject) {
+                  return DropdownMenuItem<String>(
+                    value: subject,
+                    child: Text(subject),
+                  );
+                }).toList(),
+              ),
+              SizedBox(height: 20),
+              Text(
+                'Message',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              TextFormField(
+                maxLines: 5,
+                maxLength: 256,
+                decoration: InputDecoration(
+                  hintText: 'Enter your review here',
+                  border: OutlineInputBorder(),
                 ),
-                DropdownButtonFormField<String>(
-                  value: _selectedSubject,
-                  onChanged: (String? value) {
-                    setState(() {
-                      _selectedSubject = value!;
-                    });
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Please enter a message';
+                  }
+                  return null;
+                },
+                onChanged: (value) {
+                  setState(() {
+                    _message = value;
+                  });
+                },
+              ),
+              SizedBox(height: 20),
+              Center(
+                child: ElevatedButton(
+                  onPressed: () {
+                    _submitForm();
                   },
-                  items: ['Suggestion', 'Compliment', 'Bug', 'Feature Request']
-                      .map((String subject) {
-                    return DropdownMenuItem<String>(
-                      value: subject,
-                      child: Text(subject),
-                    );
-                  }).toList(),
+                  child: Text('Submit'),
                 ),
-                SizedBox(height: 20),
-                Text(
-                  'Message',
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                ),
-                TextFormField(
-                  maxLines: 5,
-                  maxLength: 256,
-                  decoration: InputDecoration(
-                    hintText: 'Enter your review here',
-                    border: OutlineInputBorder(),
-                  ),
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return 'Please enter a message';
-                    }
-                    return null;
-                  },
-                  onChanged: (value) {
-                    setState(() {
-                      _message = value;
-                    });
-                  },
-                ),
-                SizedBox(height: 20),
-                Center(
-                  child: ElevatedButton(
-                    onPressed: () {
-                      _submitForm();
-                    },
-                    child: Text('Submit'),
-                  ),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
+      ),
+    );
+  }
+
+  Widget _buildAdminPage() {
+    return Center(
+      child: Text(
+        'Admin Page',
+        style: TextStyle(fontSize: 24),
       ),
     );
   }
@@ -160,5 +189,11 @@ class _ReviewFormState extends State<ReviewForm> {
         duration: Duration(seconds: 3),
       ),
     );
+  }
+
+  void _onTabSelected(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
   }
 }
